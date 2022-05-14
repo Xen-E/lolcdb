8f2675bb5d6cde3ecbc20a52ad5a29f7987538a2
xen-e <xen-dev@pm.me>
2022-05-14 07:51:56 +0200

    I forgot to comment the configure flags

--- build_static-small_size.bat
+++ build_static-small_size.bat
@@ -2,10 +2,10 @@
 ::If you want to use it make sure to replace the path in "-L" flag with your static CURL library
 ::Note: If you want to reach a very small size, then you should build the library only with basic/core functions
 ::Try this: ./configure --disable-shared --enable-static \
---disable-dependency-tracking --enable-ipv6 --disable-ftp --disable-file \
---disable-ldap --disable-ldaps --disable-rtsp --disable-proxy --disable-dict \
---disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smtp \
---disable-gopher --disable-sspi --disable-manual --disable-zlib --without-zlib
+::--disable-dependency-tracking --enable-ipv6 --disable-ftp --disable-file \
+::--disable-ldap --disable-ldaps --disable-rtsp --disable-proxy --disable-dict \
+::--disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smtp \
+::--disable-gopher --disable-sspi --disable-manual --disable-zlib --without-zlib
 ::This will produce a very small static CURL library for your future projects.
 ::
 :: <3 Xen <xen-dev@pm.me> 2022
